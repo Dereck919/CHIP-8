f9a94db5c06aeb08453ff4b3f61b78aecbaec87a
Dereck919 <kirkmandereck@gmail.com>
2026-03-13 13:48:13 -0700

    implementing opcodes

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -4,7 +4,6 @@
 #include<fstream>
 
 /*
-REQUIREMENTS
 Sixteen 8-bit registers (V0-VF)
 16-bit index register 
 64-byte stack with 8-bit stack pointer 
@@ -22,10 +21,10 @@ class Chip8{
     public:
     uint8_t registers[8]{};
     uint8_t memory[4096]{};
-    uint16_t ir{};
-    uint16_t pc{};
+    uint16_t ir{}; //index register
+    uint16_t pc{}; //program counter 
     uint16_t stack[16]{};
-    uint8_t sp{};
+    uint8_t sp{};  //stack pointer
     uint8_t  delayTimer{};
     uint8_t soundTimer{};
     uint16_t inputKeys[16]{};
@@ -80,12 +79,153 @@ class Chip8{
     //5xy0 - SE Vx, Vy
     void Chip8::OP_5xy0(){
         uint8_t Vx = (opcode & 0x0F00u) >> 8u;
-        uint8_t Vy = (opcode & 0x0F00u) >> 4u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
 
         if(registers[Vx] == registers[Vy]){
             pc+=2;
         }
     }
+
+    //6xkk - LD Vx, byte
+    void Chip8::OP_6xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t kk = (opcode & 0x00FFu);
+
+        registers[Vx] = kk;
+    }
+
+    //7xkk - ADD Vx, byte 
+    void Chip8::OP_7xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t kk = (opcode & 0x00FFu);
+
+        registers[Vx] = registers[Vx] + kk;
+    }
+
+    //8xy0 - LD Vx, Vy
+    void Chip8::OP_8xy0(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] = registers[Vy];
+    }
+
+    //8xy1 - OR Vx, Vy
+    void Chip8::OP_8xy1(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] = registers[Vx] | registers[Vy];
+    }
+
+    //8xy2 - AND Vx, Vy
+    void Chip8::OP_8xy2(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] = registers[Vx] & registers[Vy];
+    }
+
+    //8xy3 - XOR Vx, Vy
+    void Chip8::OP_8xy3(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] = registers[Vx] ^ registers[Vy];
+    }
+
+    //8xy4 - ADD Vx, Vy 
+    void Chip8::OP_8xy4(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        uint16_t sum = registers[Vx] + registers[Vy];
+
+        if (sum > 255u){
+            registers[0xF] = 1;
+        }else{
+            registers[0xF] = 0;
+        }
+
+        registers[Vx] = sum;
+
+    }
+
+    //8xy5 - SUB Vx, Vy
+    void Chip8::OP_8xy5(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if (registers[Vx] > registers[Vy]){
+            registers[0xF] = 1;
+        }else{
+            registers[0xF] = 0;
+        }
+
+        registers[Vx] = registers[Vx] - registers[Vy];
+    }
+
+    //8xy6 - SHR Vx {,Vy}
+    void Chip8::OP_8xy6(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        registers[0xF] = (registers[Vx] & 0x1u);
+        registers[Vx] = registers[Vx] >> 1;
+    }
+
+    //8xy7 - SUB Vx, Vy
+    void Chip8::OP_8xy5(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if (registers[Vy] > registers[Vx]){
+            registers[0xF] = 1;
+        }else{
+            registers[0xF] = 0;
+        }
+
+        registers[Vx] = registers[Vy] - registers[Vx];
+    }
+
+    //8xyE - SHL Vx, {, Vy}
+    void Chip8::OP_8xyE(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        registers[0xF] = (registers[Vx] & 0x1u);
+        registers[Vx] = registers[Vx] << 1;
+    }
+
+    //9xy0 - SNE Vx, Vy
+    void Chip8::OP_9xy0(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if (registers[Vx] != registers[Vy]){
+            pc += 2;
+        }
+    }
+
+    //Annn - LD I, addr
+    void Chip8::OP_Annn(){
+        uint16_t addr = (opcode & 0x0FFFu);
+        ir = addr;
+    } 
+
+    //Bnnn - JP V0, addr
+    void Chip8::OP_Bnnn(){
+       uint16_t addr = (opcode & 0x0FFFu);
+       pc = addr + registers[0x0]; 
+    }
+
+    //Cxkk - RND Vx, byte
+    void Chip8::OP_Cxkk(){
+
+    }
+
+    //Dxyn - DRW Vx, Vy, nibble
+    void Chip8::OP_Dxyn(){
+
+    }
+
+    
 
 
 
